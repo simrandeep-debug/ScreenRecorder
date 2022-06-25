365140c3d13d172b8236f45397dd7d98ffca3a87
Simrandeep Singh <simrandeep.singh-ext@alstomgroup.com|>
2022-06-25 17:05:20 +0200

    Display Window created

--- screenRecorder.py
+++ screenRecorder.py
@@ -8,17 +8,9 @@ import pyautogui
 ## create a video writer object which contains 4 parameters :
 ## filename, codec, fps and resolution .
   
-# Specify name of Output file
 filename = "ScreenRecording.avi"
-  
-# Specify video codec
 codec = cv2.VideoWriter_fourcc(*"XVID")
-
-# Specify frames rate. We can choose any 
-# value and experiment with it
 fps = 60.0
-  
-# Specify resolution
 resolution = (1920, 1080)
 
 # Creating a VideoWriter object
