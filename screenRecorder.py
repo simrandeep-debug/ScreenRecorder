7b71af430a33772e297232e782b9611d52f10b0b
Simrandeep Singh <simrandeep.singh-ext@alstomgroup.com|>
2022-06-29 13:02:50 +0200

    Basic Functionality implemented

--- screenRecorder.py
+++ screenRecorder.py
@@ -40,3 +40,10 @@ while (True):
     if (cv2.waitKey(1) == ord("q") or cv2.waitKey(1) == ord('Q') ):
         out.write("q or Q pressed, halt the execution")
         break
+
+
+##### to avoid unnecessary memory utilisation 
+##### it is important to kill the object and release the memory (Video Writer object)
+
+out.release()
+cv2.destroyAllWindows() ## to destroy the GUI windows 
