a89a8b8d25eef962874f1c1a0ef4ee285972387e
Simrandeep Singh <simrandeep.singh-ext@alstomgroup.com|>
2022-06-29 12:45:53 +0200

    using pyautogui to take screenshots

--- screenRecorder.py
+++ screenRecorder.py
@@ -6,11 +6,24 @@ import cv2
 import pyautogui
 
 # display screen resolution, get it using pyautogui itself
-SCREEN_SIZE = tuple(pyautogui.size())
+sizeOfScreen = tuple(pyautogui.size())
 
 # define the codec
 # to encode/decode a data stream / media 
 codec = cv2.VideoWriter_fourcc(*"XVID")
 
-# frames per second
-fps = 12.0
+# frames per second, this value can be experimented 
+framesPerSecond = 12.0
+
+# create the video write object
+out = cv2.VideoWriter("recording_Output.avi", codec, framesPerSecond, (sizeOfScreen))
+
+##########################################################
+
+##### continue taking screenshot using pyautogui and convert that into an array 
+##### Print the array / Show the output 
+while (True):
+
+    currentScreen = pyautogui.screenshot() # Take a screenshot
+
+    frameData = numpy.array(currentScreen) # convert the screen snippet to a numpy array
